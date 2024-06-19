fffb825e125f73e78ad725e8d12b0b92d8004696
sutaaa0 <sutadev0@gmail.com>
2024-06-19 21:06:35 +0700

    configurasi

---
+++ config_dev/database_dev.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "your server";
+$username = "your username";
+$password = "your password";
+$dbname = "your database name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+return $conn;
+?>
